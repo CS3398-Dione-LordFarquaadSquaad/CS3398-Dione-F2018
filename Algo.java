0ce7ca1c11f33b7dd9ed467c02affdf804034b76
Laura D <laura.doan@yahoo.com>
2018-10-09 18:26:29 -0500

    Some bug fixes

--- Algo.java
+++ Algo.java
@@ -1,6 +1,5 @@
 /*
-Thursday 5pm check in. 
-Done with data structure, almost done with rows algorithm (debugging). Columns are will be little more than copy paste
+Still having issues
 */
 
 import java.io.FileReader;
@@ -27,7 +26,7 @@ public class Algo{
             maxR = reader.nextInt(10);
             maxC = reader.nextInt(10);
         
-            int[][] Row = new int[numR][maxR];
+            int[][] Row = new int[numC][maxR];
             for (int i = 0; i < numR; i++){
                 for (int j = 0; j < maxR; j++){
                     //read num for next point
@@ -35,7 +34,7 @@ public class Algo{
                 }
             }
         
-            int[][] Column = new int[numR][maxR];
+            int[][] Column = new int[numR][maxC];
             for (int i = 0; i < numC; i++){
                 for (int j = 0; j < maxC; j++){
                     //read num for next point
@@ -44,112 +43,152 @@ public class Algo{
             }
         
             int[][] Grid = new int[numC][numR];
-            int[][] Lvalues = new int[numC]
+            int[][] Lvalues = new int[maxR][numR];
             int[] currRparam = new int[maxR];
-            int[] currR = new int[numC];
+            int[] currR = new int[numC]; //current grid row (for comparison)
             int[] newR = new int[numC]; //replaces the current grid row after full iteration
             int[] workingR = new int[numC]; //"added" to newR if valid
             int[] currCparam = new int[maxC];
             int[] currC = new int[numR];
-            int currParam; 
+            int workingParam;
             int currParamNum; //location in currParamC or currParamR
             int loc; //location in currR or currC
             int[] ws = new int[maxR];
-            int fillstart;
-            int space;
+            int space; //number of spaces before currParam
+            int check;
             boolean done = false;
             boolean notMax = true;
             boolean good;
         
-        //CALC MAXVALUES!!
-        
-            for (int i = 0; i < numR; i++){
-                for (int j= 0; j<numC; j++){
-                    Grid[j][i]=1;
+            for (int i = 0; i < numC; i++){
+                for (int j= 0; j<numR; j++){
+                    Grid[i][j]=1;
+                }
+                
+                loc = numC;
+                for(currParamNum = maxR-1; currParamNum >= 0; currParamNum--){
+                    if(Row[i][currParamNum] != 0){
+                        Lvalues[currParamNum][i] = loc - Row[i][currParamNum];
+                        loc--;
+                    }
+                    else 
+                        Lvalues[currParamNum][i] = loc;
                 }
             }
+            
+            System.out.println(Lvalues[1][0]);
+            System.out.println(Lvalues[1][1]);
+            System.out.println(Lvalues[0][2]);
+            System.out.println(Lvalues[1][2]);
                     
             //fill Grid
             while(!done){
+                done = true;
                 
                 //for all rows 
                 for (int i = 0; i < numR; i++){
                     
                     //fill param list
-                    for (int j = 0; j < maxR; i++){
-                        currRparam[j] = Row[i][j];
+                    for ( loc = 0; loc < maxR; loc++){
+                        currRparam[loc] = Row[i][loc];
                     }
                 
                     //fill CurrR and reset newR
-                    for (int j = 0; j < numC; j++){
-                        currR[j] = Grid[j][i];
-                        newR[j] = -1;
+                    for (loc = 0; loc < numC; loc++){
+                        currR[loc] = Grid[loc][i];
+                        newR[loc] = -1;
+                        workingR[loc] = -1;
                     }
 
                     //fill row
-                    for(int currParamNum = maxR - 1; currParamNum >= 0; currParamNum--){
-                        fillstart = -1;
+                    for(currParamNum = maxR - 1; currParamNum >= 0; currParamNum--){
                         space = 0;
                         do{
+                            loc = 0;
                             for(int k = 0; k < currParamNum; k++){
-                                fillstart++;
-                                for (int currParam = currRparam[currParamNum]; currParam>0; currParam--){
-                                    workingR[fillstart] = 2;
-                                    fillstart++
+                                for (workingParam = currRparam[k]; workingParam>0; workingParam--){
+                                    workingR[loc] = 2;
+                                    loc++;
                                 }
-                                currR[fillstart] = 0
+                                if(currRparam[k] != 0){
+                                    currR[loc] = 0;
+                                    loc++;
+                                }
                             }
                             
                             for(int k = space; k > 0; k--){
-                                fillstart++
-                                workingR[fillstart] = 0;
+                                workingR[loc] = 0;
+                                 loc++;
                             }
-                            check = fillstart++;
+                            check = loc+1;
                             
-                            for(int k = ; k > 0; k--){
-                                fillstart++
-                                workingR[fillstart] = 2;
+                            for(int k = currRparam[currParamNum]; k > 0; k--){
+                                workingR[loc] = 2;
+                                loc++;
                             }
                             
-                            if(j < maxR-1)
-                                fillstart = CmaxR[j + 1];
-                            
-                            for( int k = j + 1; k < maxR; j++){
-                                for (int l = currRparam[j]; l > 0; l--){
-                                    workingR[fillstart] = 2;
-                                    fillstart++;
+                            if(currParamNum < maxR-1){
+                                int gt = Lvalues[currParamNum + 1][i];
+                                while(loc < gt){
+                                    workingR[loc] = 0;
+                                    loc++;
                                 }
-                                currRfillstart = 0;
-                                fillstart++;
+                            }
+                            
+                            for( int k = currParamNum + 1; k < maxR; k++){
+                                for (workingParam = currRparam[k]; workingParam > 0; workingParam--){
+                                    if(loc < numC){
+                                        workingR[loc] = 2;
+                                        loc++;
+                                    }
+                                }
+                                if(loc < numC){
+                                    currR[loc] = 0;
+                                    loc++;
+                                }
+                            }
+                            
+                            while( loc < numC){
+                                workingR[loc] = 0;
+                                loc++;
                             }
                             
                             good = true;
-                            for( loc = 0; loc<numC && good; loc++){
+                            for( loc = 0; loc<numC /*&& good*/; loc++){
+                                System.out.print(workingR[loc]);
                                 if(currR[loc] != 1){
                                     if(currR[loc] != workingR[loc]){
                                         good = false;
                                     }
                                 }
                             }
+                            System.out.println("");
                             
                             if(good){
+                                done = false;
                                 for( loc = 0; loc < numC; loc++){
-                                    if(newR[loc] = -1)
+                                    if(newR[loc] == -1)
                                         newR[loc] = workingR[loc];
                                     else if(workingR[loc] != newR[loc])
                                         newR[loc] = 1;
                                 }
                             }
-                            space++
-                        }while( (check+1)>= CmaxR[currParamNum])
-                    for(int l = 0; for l < numC; l++){    
-                        grid[l][currParamnum]
+                            space++;
+                        }while( check <= Lvalues[currParamNum][i] && currR[currParamNum] != 0);
+                        
+                        System.out.println("Row# " + i);
+                        for(loc = 0; loc < numC; loc++){    
+                            Grid[loc][i] = newR[loc];
+                            System.out.print(Grid[loc][i]);
+                        }
+                        System.out.print("\n \n");
                     }
                 }
+                done = true;
             }
-        
         }
         catch(Exception e){
+            System.out.println("error: " + e);
         }
         
     }
