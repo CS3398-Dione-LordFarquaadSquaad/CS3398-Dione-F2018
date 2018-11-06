b653c8a7bf0e6d6f1219390d86057b827bf7d109
Laura D <laura.doan@yahoo.com>
2018-11-06 16:56:30 -0600

    Finished Algorithm

--- Algo.java
+++ Algo.java
@@ -44,10 +44,13 @@ public class Algo{
         
             int[][] Grid = new int[numC][numR];
             int[][] Lvalues = new int[maxR][numR];
+            int[][] Dvalues = new int[maxC][numC];
             int[] currRparam = new int[maxR];
             int[] currR = new int[numC]; //current grid row (for comparison)
             int[] newR = new int[numC]; //replaces the current grid row after full iteration
+            int[] newC = new int[numR];
             int[] workingR = new int[numC]; //"added" to newR if valid
+            int[] workingC = new int[numR];
             int[] currCparam = new int[maxC];
             int[] currC = new int[numR];
             int workingParam;
@@ -64,6 +67,7 @@ public class Algo{
                 for (int j= 0; j<numR; j++){
                     Grid[i][j]=1;
                 }
+            
                 
                 for(currParamNum = maxR-1; currParamNum >= 0; currParamNum--){
                     if(Row[i][currParamNum] != 0){
@@ -75,6 +79,19 @@ public class Algo{
                     }
                     else 
                         Lvalues[currParamNum][i] = 0;
+                }
+            
+            
+                for(currParamNum = maxC-1; currParamNum >= 0; currParamNum--){
+                    if(Column[i][currParamNum] != 0){
+                        Dvalues[currParamNum][i] = numR - Column[i][currParamNum];
+                        int temp = currParamNum+1;
+                        while(temp <= maxC-1){
+                            Dvalues[currParamNum][i] -= Column[i][temp++]+1;
+                        }
+                    }
+                    else 
+                        Dvalues[currParamNum][i] = 0;
                 }
             }
             
@@ -156,18 +173,18 @@ public class Algo{
                             }
                             
                             good = true;
-                            for( loc = 0; loc<numC /*&& good*/; loc++){
-                                System.out.print(workingR[loc]);
-                                if(currR[loc] != 1){
+                            for( loc = 0; loc<numC && good; loc++){
+                                //System.out.print(workingR[loc]);
+                                if(currR[loc] != 1 && currR[loc] != -1){
                                     if(currR[loc] != workingR[loc]){
                                         good = false;
                                     }
                                 }
                             }
-                            System.out.println("");
+                            //System.out.println("");
                             
                             if(good){
-                                done = false;
+                                //done = false;
                                 for( loc = 0; loc < numC; loc++){
                                     if(newR[loc] == -1)
                                         newR[loc] = workingR[loc];
@@ -178,15 +195,135 @@ public class Algo{
                             space++;
                         }}while( check <= Lvalues[currParamNum][i]);
                         
-                        System.out.println("Row# " + i);
+                        //System.out.println("Row# " + i);
                         for(loc = 0; loc < numC; loc++){    
-                            Grid[loc][i] = newR[loc];
-                            System.out.print(Grid[loc][i]);
+                            if(newR[loc] != -1){
+                                Grid[loc][i] = newR[loc];
+                                if(Grid[loc][i] == 1)
+                                    done = false;
+                            }
+                            //System.out.print(Grid[loc][i]);
                         }
-                        System.out.print("\n \n");
+                        //System.out.print("\n \n");
                     }
                 }
-                done = true;
+                
+                //for all columns
+                for (int i = 0; i < numC; i++){
+                    
+                    //fill param list
+                    for ( loc = 0; loc < maxC; loc++){
+                        currCparam[loc] = Column[i][loc];
+                    }
+                
+                    //fill CurrC and reset newC
+                    for (loc = 0; loc < numR; loc++){
+                        currC[loc] = Grid[i][loc];
+                        //System.out.print(Grid[i][loc]);
+                        newC[loc] = -1;
+                        workingC[loc] = -1;
+                    }
+                    //System.out.println("");
+
+                    //fill column
+                    for(currParamNum = maxC - 1; currParamNum >= 0; currParamNum--){
+                        space = 0;
+                        check = 999;
+                        do{ if(currCparam[currParamNum] != 0){
+                            loc = 0;
+                            for(int k = 0; k < currParamNum; k++){
+                                workingParam = currCparam[k];
+                                for (; workingParam>0; workingParam--){
+                                    if(loc+workingParam <= Dvalues[k][i]){
+                                        workingC[loc] = 2;
+                                        loc++;
+                                    }
+                                    else break;
+                                }
+                                if(currCparam[k] != 0){
+                                    workingC[loc] = 0;
+                                    loc++;
+                                }
+                            }
+                            
+                            for(int k = space; k > 0; k--){
+                                workingC[loc] = 0;
+                                 loc++;
+                            }
+                            check = loc+1;
+
+                            for(int k = currCparam[currParamNum]; k > 0; k--){
+                                    workingC[loc] = 2;
+                                    loc++;
+                            }
+                            
+                            if(currParamNum < maxC-1){
+                                int gt = Dvalues[currParamNum + 1][i];
+                                while(loc < gt){
+                                    workingC[loc] = 0;
+                                    loc++;
+                                }
+                            }
+                            
+                            for( int k = currParamNum + 1; k < maxC; k++){
+                                for (workingParam = currCparam[k]; workingParam > 0; workingParam--){
+                                    if(loc < numR){
+                                        workingC[loc] = 2;
+                                        loc++;
+                                    }
+                                }
+                                if(loc < numR){
+                                    currC[loc] = 0;
+                                    loc++;
+                                }
+                            }
+
+                            while( loc < numR){
+                                workingC[loc] = 0;
+                                loc++;
+                            }
+                            
+                            good = true;
+                            for( loc = 0; loc<numR && good; loc++){
+                                //System.out.print(workingC[loc]);
+                                if(currC[loc] != 1 && currC[loc] != -1){
+                                    if(currC[loc] != workingC[loc]){
+                                        good = false;
+                                    }
+                                }
+                            }
+                            //System.out.println("");
+                            
+                            if(good){
+                                //done = false;
+                                for( loc = 0; loc < numR; loc++){
+                                    if(newC[loc] == -1)
+                                        newC[loc] = workingC[loc];
+                                    else if(workingC[loc] != newC[loc])
+                                        newC[loc] = 1;
+                                }
+                            }
+                            space++;
+                        }}while( check <= Dvalues[currParamNum][i]);
+                        
+                        //System.out.println("Column# " + i);
+                        for(loc = 0; loc < numR; loc++){    
+                            if(newC[loc] != -1){
+                                Grid[i][loc] = newC[loc];
+                                if(Grid[i][loc] == 1)
+                                    done = false;
+                            }
+                            //System.out.print(Grid[i][loc]);
+                        }
+                        //System.out.print("\n \n");
+                    }
+                }
+            }
+            for(int c = 0; c < numC; c++){
+                for(int r = 0; r < numR; r++){
+                    System.out.print(Grid[c][r]);
+                }
+                System.out.println("");
             }
         }
         catch(Exception e){
